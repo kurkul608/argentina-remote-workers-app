1380910f03e13e84c5c47ece7cfc559ef4ee2bd5
Petr Molchanov <p.molchanov@profsoft.pro>
2022-10-22 00:36:13 -0300

    add message send method

--- backend/src/bots/main-bot/services/send-message.service.ts
+++ backend/src/bots/main-bot/services/send-message.service.ts
@@ -2,7 +2,7 @@ import TelegramBot from "node-telegram-bot-api";
 
 export const sendMessageService = (
   bot: TelegramBot,
-  chatId: string,
+  chatId: string | number,
   message: string
 ) => {
   return bot.sendMessage(chatId, message);
