6ecca7020639ed256038ef734994cbad62967759
Solpio <ssusolpio@gmail.com>
2022-10-27 21:04:01 +0400

    feat: add styles

--- admin-app/src/shared/chat-list/components/chat-list-widget/index.tsx
+++ admin-app/src/shared/chat-list/components/chat-list-widget/index.tsx
@@ -1,7 +1,14 @@
 import React, { useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "../../../../redux/hooks";
 import { Widget } from "../../../widget";
-import { ChatListUL } from "./styled";
+import {
+  Chat,
+  ChatListUL,
+  ChatListWrapper,
+  ChatPhoto,
+  ChatPhotoWrapper,
+  ChatTitle,
+} from "./styled";
 import { getChatsList } from "../../services/data";
 import { getAllChats } from "../../redux/chat-list.slice";
 
@@ -9,19 +16,47 @@ export const ChatListWidget = () => {
   const chatList = useAppSelector((state) => state.chats.list);
   const dispatch = useAppDispatch();
 
+  const item = [
+    {
+      id: 1,
+      title: "Гейпорно",
+    },
+    {
+      id: 2,
+      title: "Я люблю большие члены",
+    },
+    {
+      id: 3,
+      title: "Школа #31",
+    },
+    {
+      id: 4,
+      title: "Дора",
+    },
+  ];
+
   useEffect(() => {
     getChatsList().then((data) => console.log(data));
     dispatch(getAllChats());
   }, []);
   return (
     <>
-      <Widget name={"Chat list widget"}>
-        <ChatListUL>
-          {chatList.map((chat) => (
-            <li key={`widget-chat-list--${chat.id}`}>{chat.title}</li>
-          ))}
-        </ChatListUL>
-      </Widget>
+      <ChatListWrapper>
+        <Widget name={"Chat list widget"}>
+          <ChatListUL>
+            {item.map((chat) => (
+              <li key={`widget-chat-list--${chat.id}`}>
+                <Chat>
+                  <ChatPhotoWrapper>
+                    <ChatPhoto>{chat.title[0].toUpperCase()}</ChatPhoto>
+                  </ChatPhotoWrapper>
+                  <ChatTitle>{chat.title}</ChatTitle>
+                </Chat>
+              </li>
+            ))}
+          </ChatListUL>
+        </Widget>
+      </ChatListWrapper>
     </>
   );
 };
--- admin-app/src/shared/chat-list/components/chat-list-widget/styled.ts
+++ admin-app/src/shared/chat-list/components/chat-list-widget/styled.ts
@@ -1,9 +1,48 @@
 import styled from "styled-components";
 import { color } from "../../../../constants/colors";
 
+export const ChatListWrapper = styled.div`
+  max-width: 280px;
+  width: 100%;
+`;
+
 export const ChatListUL = styled.ul`
+  margin-top: 10px;
   li {
-    font-size: 12px;
+    margin-top: 10px;
+    font-size: 1.4rem;
+    padding: 5px;
     color: ${(props) => color(props.theme.mainTheme).widgetMainText};
+    &:hover {
+      background-color: #545454;
+    }
   }
+`;
+
+export const Chat = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+export const ChatTitle = styled.h3`
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+`;
+export const ChatPhoto = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
+  text-align: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 100px;
+  background-color: silver;
+  border: 1px black solid;
+`;
+
+export const ChatPhotoWrapper = styled.div`
+  position: relative;
+  //border: 1px solid red;
 `;
