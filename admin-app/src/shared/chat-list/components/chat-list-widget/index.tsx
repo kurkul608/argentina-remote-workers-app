cda97375db9baba844dbb7dba48ec94a384b4968
Solpio <ssusolpio@gmail.com>
2022-11-03 10:00:09 +0400

    feat: pull chat-list-widget

--- admin-app/src/shared/chat-list/components/chat-list-widget/index.tsx
+++ admin-app/src/shared/chat-list/components/chat-list-widget/index.tsx
@@ -2,15 +2,12 @@ import React, { useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "../../../../redux/hooks";
 import { Widget } from "../../../widget";
 import { ChatListWrapper } from "./styled";
-import { getChatsList } from "../../services/data";
 import { getAllChats } from "../../redux/chat-list.slice";
 
 export const ChatListWidget = () => {
   const chatList = useAppSelector((state) => state.chats.list);
   const dispatch = useAppDispatch();
-
   useEffect(() => {
-    getChatsList().then((data) => console.log(data));
     dispatch(getAllChats());
   }, []);
   return (
