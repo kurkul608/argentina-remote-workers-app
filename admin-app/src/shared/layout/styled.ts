c5d673f6ae45789a51f03f71e23bb5ae900eb3a2
Solpio <ssusolpio@gmail.com>
2022-11-04 23:09:56 +0400

    feat: add grid

--- admin-app/src/shared/layout/styled.ts
+++ admin-app/src/shared/layout/styled.ts
@@ -1,9 +1,31 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
+  width: 100%;
   padding: 25px;
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  align-items: center;
   gap: 25px;
   row-gap: 25px;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-flow: row dense;
+  @media (max-width: 1900px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 320px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+export const MainWrapper = styled.div`
+  //padding: 25px;
+  //display: grid;
+  //justify-content: start;
+  //justify-items: start;
+  //gap: 25px;
+  //row-gap: 25px;
+  //grid-template-columns: 1fr;
 `;
