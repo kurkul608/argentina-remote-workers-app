a7fec8f736de69424f1d50a46f7c54149c3667f3
Solpio <ssusolpio@gmail.com>
2022-11-17 00:26:41 +0400

    feat: new route

--- admin-app/src/shared/router/index.tsx
+++ admin-app/src/shared/router/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { AuthPage } from "../../pages/auth-page";
-import { MainPage } from "../../pages/main-page";
-import { Layout } from "../layout";
+import {Routes, Route} from "react-router-dom";
+import {AuthPage} from "../../pages/auth-page";
+import {MainPage} from "../../pages/main-page";
+import {Layout} from "../layout";
+import {ChatPage} from "../../pages/chat-page";
 
 const NotFound = () => {
   return <div>Page not found</div>;
@@ -10,12 +11,13 @@ const NotFound = () => {
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path={"/"} element={<Layout />}>
-        <Route index element={<MainPage />} />
-        <Route path={"test"} element={<div>Test content</div>} />
+      <Route path={"/"} element={<Layout/>}>
+        <Route index element={<MainPage/>}/>
+        <Route path={"test"} element={<div>Test content</div>}/>
+        <Route path={"chat/:chatId"} element={<ChatPage/>}/>
       </Route>
-      <Route path={"auth"} element={<AuthPage />} />
-      <Route path={"*"} element={<NotFound />} />
+      <Route path={"auth"} element={<AuthPage/>}/>
+      <Route path={"*"} element={<NotFound/>}/>
     </Routes>
   );
 };
