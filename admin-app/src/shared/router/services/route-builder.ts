81192ba7318164da2465c43c55d87da00d0d053d
Petr Molchanov <p.molchanov@profsoft.pro>
2023-01-16 14:13:53 -0300

    https://argentinadev.atlassian.net/browse/RA-4

--- admin-app/src/shared/router/services/route-builder.ts
+++ admin-app/src/shared/router/services/route-builder.ts
@@ -1,5 +1,6 @@
 import { Routes } from "../index";
+import { Pathname } from "react-router-dom";
 
-export const routeBuilder = (routes: Array<Routes>): string => {
+export const routeBuilder = (routes: Array<Routes>): Pathname => {
   return routes.reduce((acc, route) => `${acc}/${route}`, "");
 };
