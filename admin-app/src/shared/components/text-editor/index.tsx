f1ee34615f279f3d88a68f3b6c4a7dadc991bfe6
Petr Molchanov <p.molchanov@profsoft.pro>
2023-01-18 00:00:47 -0300

    https://argentinadev.atlassian.net/browse/RA-17

---
+++ admin-app/src/shared/components/text-editor/index.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+export const TextEditor = () => {
+	const [value, setValue] = useState("");
+	return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+};
