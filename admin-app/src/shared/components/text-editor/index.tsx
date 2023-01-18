3382f187852561deb8a76e74ed6c8e469a9d90c5
Petr Molchanov <p.molchanov@profsoft.pro>
2023-01-18 00:20:54 -0300

    https://argentinadev.atlassian.net/browse/RA-17

--- admin-app/src/shared/components/text-editor/index.tsx
+++ admin-app/src/shared/components/text-editor/index.tsx
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
-import ReactQuill from "react-quill";
+import React from "react";
+import ReactQuill, { UnprivilegedEditor } from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { DeltaStatic, Sources } from "quill";
 
-export const TextEditor = () => {
-	const [value, setValue] = useState("");
-	return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+interface IOwnProps {
+	value: string;
+	onChange(
+		value: string,
+		delta: DeltaStatic,
+		source: Sources,
+		editor: UnprivilegedEditor
+	): void;
+	id: string;
+}
+export const TextEditor = ({ value, onChange, id }: IOwnProps) => {
+	return <ReactQuill theme="snow" value={value} onChange={onChange} id={id} />;
 };
