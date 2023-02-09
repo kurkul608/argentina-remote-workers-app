bf5bbb7370fb7b96e6aa91467ef4d42673dc3b88
Solpio <ssusolpio@gmail.com>
2023-02-09 16:32:19 +0400

    https://argentinadev.atlassian.net/browse/RA-31 | add checkAuth method

--- admin-app/src/shared/auth/redux/auth.slice.ts
+++ admin-app/src/shared/auth/redux/auth.slice.ts
@@ -1,13 +1,28 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { checkAuth } from "shared/auth/services/checkAuth";
 
 export interface IAuth {
 	token?: string | null;
 	status: "loading" | "fulfilled" | "rejected";
+	isExpired: boolean;
 }
 
 export const initialState: IAuth = {
 	status: "loading",
+	isExpired: false,
 };
+
+interface IErrorAuthMessage {
+	message: string;
+	statusCode: number;
+}
+
+export const checkAuthAsync = createAsyncThunk(
+	"auth/check",
+	async (token: string) => {
+		return (await checkAuth(token)) as IErrorAuthMessage | boolean;
+	}
+);
 
 const AuthSlice = createSlice({
 	name: "auth",
@@ -19,6 +34,16 @@ const AuthSlice = createSlice({
 		logOut: (state) => {
 			state.token = undefined;
 		},
+	},
+	extraReducers: (builder) => {
+		builder.addCase(checkAuthAsync.fulfilled, (state, action) => {
+			if (typeof action.payload === "boolean") {
+				state.isExpired = false;
+			} else {
+				state.isExpired = true;
+				state.token = undefined;
+			}
+		});
 	},
 });
 
--- admin-app/src/shared/router/private/private-route.tsx
+++ admin-app/src/shared/router/private/private-route.tsx
@@ -1,14 +1,26 @@
-import { useAppSelector } from "redux/hooks";
-import React, { lazy, Suspense } from "react";
+import { useAppDispatch, useAppSelector } from "redux/hooks";
+import React, { lazy, Suspense, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { localStorageServiceGet } from "services/local-storage.service";
+import { checkAuthAsync } from "shared/auth/redux/auth.slice";
 const Layout = lazy(() =>
 	import("shared/layout").then((module) => ({ default: module.Layout }))
 );
 export const PrivateRoute = () => {
-	const { token } = useAppSelector((state) => state.auth);
+	const dispatch = useAppDispatch();
+	const { token, isExpired } = useAppSelector((state) => state.auth);
 	const localStorageToken = localStorageServiceGet("auth");
-	return token || localStorageToken ? (
+	useEffect(() => {
+		if (localStorageToken) {
+			dispatch(checkAuthAsync(localStorageToken));
+		}
+	}, [localStorageToken]);
+	useEffect(() => {
+		if (isExpired) {
+			localStorage.removeItem("auth");
+		}
+	}, [isExpired]);
+	return (token || localStorageToken) && !isExpired ? (
 		<Suspense fallback={<>...</>}>
 			<Layout />
 		</Suspense>
